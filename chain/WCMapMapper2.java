3af757eaa31fe3c1c5f727b81ddca16cba31a180
tonykidkid <2564126274@qq.com>
2017-03-23 19:12:57 +0800

    added

--- chain/WCMapMapper2.java
+++ chain/WCMapMapper2.java
@@ -10,7 +10,7 @@ import java.io.IOException;
  *
  */
 public class WCMapMapper2 extends Mapper<Text, IntWritable, Text, IntWritable>{
-	// 第二次map()做映射，目的是过滤掉敏感词
+    // 第二次map()做映射，目的是过滤掉敏感词
     protected void map(Text key, IntWritable value, Context context) throws IOException, InterruptedException {
         if(!key.toString().equals("falungong")){
             context.write(key,value);
